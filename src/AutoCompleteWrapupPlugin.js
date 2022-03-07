3e2ba88a0c652db9ed86235344d3277bc0d6c4da
Cheryl Jose <cherylmj01@gmail.com>
2022-03-07 10:46:09 -0800

    Upgraded the plugin to the latest version

--- src/AutoCompleteWrapupPlugin.js
+++ src/AutoCompleteWrapupPlugin.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { VERSION } from '@twilio/flex-ui';
-import { FlexPlugin } from 'flex-plugin';
+import { FlexPlugin } from '@twilio/flex-plugin';
 import ConfigureFlexStrings from './flex-hooks/strings';
 import RegisterFlexNotifications from './flex-hooks/notifications';
 import FlexState from './states/FlexState';
